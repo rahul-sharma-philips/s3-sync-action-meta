b15caf55c38526382ee54d386a2f5525b81d987d
Rahul Sharma <86104001+rahul-sharma-philips@users.noreply.github.com>
2024-08-10 12:13:05 +0530

    Update upload_artifactory_to_s3.py

--- upload_artifactory_to_s3.py
+++ upload_artifactory_to_s3.py
@@ -65,10 +65,16 @@ def upload_file_to_s3(file_url, s3_bucket, s3_key):
 def main():
     repo_url = f"{ARTIFACTORY_URL}/{ARTIFACTORY_REPO}"
     files = get_artifactory_files(repo_url)
+    print("files...", files)
 
     for file_uri in files:
+        print("file_uri...", file_uri)
         file_url = f"{repo_url}/{file_uri}" #f"{repo_url}{file_uri}"
-        s3_key = f"{ARTIFACTORY_REPO}{file_uri}" #file_uri.lstrip('/')  # Adjust as necessary for your S3 structure
+        print("file_url...", file_url)
+        s3_key = f"{ARTIFACTORY_REPO}/{file_uri}" #file_uri.lstrip('/')  # Adjust as necessary for your S3 structure
+        print("s3_key...", s3_key)
+        print("AWS_S3_BUCKET...", AWS_S3_BUCKET)
+        
         upload_file_to_s3(file_url, AWS_S3_BUCKET, s3_key)
 
 if __name__ == "__main__":
